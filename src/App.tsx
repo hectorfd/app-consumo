761ffbfccf496b2b42be36fe564a493a4083c485
Hector <61565698+hectorfd@users.noreply.github.com>
2024-11-03 14:48:59 -0500

    preparando app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 
-
-
 function App() {
-
-
   return (
     <>
-    <h1 className=" text-7xl font-bold">Hola mundo</h1>
+      <header className="bg-fuchsia-600 py-4">
+        <h1 className="text-center text-fuchsia-100 font-bold text-3xl">App de Consumo</h1>
+      </header>
+      <main className="max-w-5xl mx-auto py-4 grid md:grid-cols-2">
+        <div>
+          <h2 className="font-bold text-2xl">Productos</h2>
+        </div>
+        <div>
+          <h2 className="font-bold text-2xl">Consumo</h2>
+        </div>
+
+      </main>
+
     </>
   )
 }
